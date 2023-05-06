da2751e4898b4a5c1036bb06c6d5fed7047ec2bd
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-06 18:51:16 +0100

    removed the debug banner in the application \ added stateful widegt to the home_Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
