77673fbf4e5d5ef509e1b283d9f22ee5e63e2d93
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-06 23:30:34 +0100

    added the heading text of the application

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,10 +17,32 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
+        ),
         child: Column(
           children: [
             searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 50,
+                      bottom: 20,
+                    ),
+                    child: Text(
+                      'All ToDos',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
