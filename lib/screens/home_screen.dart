0e1c8b2e1c242693a8737e85dafd50b6cecb0ccb
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-06 21:41:26 +0100

    made some extra configuration on the app bar and extracted has a method

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:todo_app_tutorial/constant/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,21 +14,32 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(
-              Icons.menu,
-              size: 30,
-            ),
-            Container(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            size: 30,
+            color: tdBlack,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
               height: 40,
               width: 40,
+              color: Colors.grey,
               child: Icon(Icons.person),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
